97d48a19ab68ac474a1290e140385808879d5d4b
Kashif Hussain <kh9732494@gmail.com>
2023-08-16 09:55:30 +0500

    JS lecture 6

---
+++ Class-09April/chp54.js
@@ -0,0 +1,19 @@
+function facebook() {
+  var link = document.getElementById("fb");
+  var address = link.href;
+  alert(address);
+}
+
+function makeItBig() {
+    document.getElementById("big").style.fontSize="5rem";
+    document.getElementById("big").style.float="center";
+    document.getElementById("big").style.color="gray";
+    document.getElementById("big").style.fontWeight="bold";
+    document.getElementById("big").style.backgroundColor="yellow";
+    document.getElementById("big").style.borderRadius="10px";
+    document.getElementById("big").style.border="black";
+    var par = document.getElementsByTagName("p");
+    alert(par[0].innerHTML);
+
+    document
+}
