54d5002045cc200b6e8d70fc79ffe76b831b2be6
Kashif Hussain <kh9732494@gmail.com>
2023-08-16 09:53:51 +0500

    JS lecture 2

---
+++ Class-2April/eventButtons.js
@@ -0,0 +1,24 @@
+// function onFocusChangeColor() {
+//   document.getElementById("email").style.backgroundColor = "red";
+// }
+
+function onClickShowAlert() {
+alert("Hello World! This message is shown is using onClick* event")
+}
+
+// function onBlurChnageColor() {
+//   document.getElementById("email").style.backgroundColor = "yellow";
+// }
+
+
+// function onFocusChangeColor(fieldID) {
+//     document.getElementById(fieldID).style.backgroundColor = "red";
+//   }
+  
+//   function onBlurChnageColor(fieldId) {
+//     document.getElementById(fieldId).style.backgroundColor = "yellow";
+//   }
+
+// function onMouseOver(field) {
+//     document.getElementById(field).style.backgroundColor = "green";
+//}
